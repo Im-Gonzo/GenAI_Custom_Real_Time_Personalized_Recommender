0455c3eaeac822e251107ac8557414fcdb05b01f
Gonzalo Alcala <gonzaloalcala0304@gmail.com>
2025-02-18 15:07:08 +0100

    god help

--- recsys/gcp/feature_store/__init__.py
+++ recsys/gcp/feature_store/__init__.py
@@ -1,31 +1,12 @@
 """
 Feature Store integration utilities.
 """
-from .client import (
-    initialize_feature_store,
-    get_client,
-    get_feature_views
-)
-from .datasets import (
-    create_training_dataset,
-    create_ranking_dataset
-)
-from .config import (
-    FEATURE_CONFIGS,
-    get_feature_config
-)
+from . import client
+from . import datasets
+from . import config
 
 __all__ = [
-    # Client
-    'initialize_feature_store',
-    'get_client',
-    'get_feature_views',
-    
-    # Datasets
-    'create_training_dataset',
-    'create_ranking_dataset',
-    
-    # Config
-    'FEATURE_CONFIGS',
-    'get_feature_config'
+    'client',
+    'datasets',
+    'config'
 ]
